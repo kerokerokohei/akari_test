8f2a0e468e795a4f511fc201f57d2f7db3356f9d
kerokerokohei <135269005+kerokerokohei@users.noreply.github.com>
2024-04-10 00:51:21 +0900

    [CHANGE]：updatetaskのロジックを変更

--- src/modules/task/dto/update-task.input.ts
+++ src/modules/task/dto/update-task.input.ts
@@ -5,4 +5,13 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 export class UpdateTaskInput extends PartialType(CreateTaskInput) {
   @Field(() => Int)
   id: number;
+
+  @Field()
+  taskname: string;
+
+  @Field()
+  complete: boolean;
+
+  @Field()
+  Isdeleted: boolean;
 }
