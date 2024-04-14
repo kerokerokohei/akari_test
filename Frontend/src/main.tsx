9c95a508d9619ea427e2be5b1f27d6bbb0a23ece
kerokerokohei <135269005+kerokerokohei@users.noreply.github.com>
2024-04-14 21:49:33 +0900

    [ADD]：main.tsxにApolloclientインスタンスを追加

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql", // GraphQLエンドポイント
+  cache: new InMemoryCache(),
+});
+
+export default client;
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+);
